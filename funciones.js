c718d8b01d699ea80c10bacd766048e866f6f728
Ronconi Elisa María <132313233+ElisaRonconi@users.noreply.github.com>
2024-04-04 21:29:36 -0300

    botones
    saludar, sumar y comparar

---
+++ funciones.js
@@ -0,0 +1,23 @@
+function saludar (NOMBRE){
+    VAR= NOMBRE= 'ELISA';
+     alert ("HOLA:"+ NOMBRE);
+ }
+ onclick
+
+function resultado (resultado){
+   const numero1= 30;
+    const numero2= 15;
+    const resultado= numero1+numero1;
+    alert ("El resultado es:"+ (resultado));
+}
+onclick
+
+function EsMayor (condicion){
+    var numero= 30;
+    const condicion=50;
+  if (num>condicion)  
+  alert ("El numero es mayor")
+  else 
+  if (num<condicion)  
+  alert ("El numero NO es mayor")
+}
